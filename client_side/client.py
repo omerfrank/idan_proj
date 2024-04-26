8a6b79cf25b3eb3f47e8052596a8d99800c095e4
omer frank <omerfrank6@gmail.com>
2024-04-26 21:01:42 +0300

    workiing imeges + videos

--- client_side/client.py
+++ client_side/client.py
@@ -13,20 +13,40 @@ class MyHandler(BaseHTTPRequestHandler):
         Handles GET requests (serving static files).
         """
         requested_url = self.path
+        print(f"\n requested_url: {requested_url} \n")
         #if requested_url == '/':
-        requested_url = '/index.html'  # Serve index.html for root path
 
         # Construct the absolute path to the requested file
-        full_path = "client_side\index.html"
-        print(full_path)
-
+        full_path = "client_side"
         try:
-            with open(full_path, 'rb') as f:
+            filetype = self.path.split('.')[1]# if self.path != '/' else 'html'
+            filename = self.path.split('.')[0]# if self.path != '/' else 'index'
+            if filetype in ["jpg", "gif"]:
+                content_type = "image/jpeg"
+            elif filetype == "html":
+                content_type = 'text/html; charset=UTF-8'
+            elif filetype == 'css':
+                content_type ="Content-Type: text/css"
+            elif filetype in ['mp4','WebM','OGG']:
+                content_type =f"Content-Type: video/{filetype}"
+            elif filetype == 'ico':
+                content_type = "Content-Type: image/x-icon;"
+            else:
+                self.send_error(415, 'unsupported media type ')
+                return
+        except Exception as e:
+            # Handle other potential errors
+            print(f"Error serving file: {e}")
+            self.send_error(500, 'Internal Server Error')            
+        try:
+            self.send_response(200)  # OK status code
+            self.send_header('Content-Type', content_type)
+            self.end_headers()
+            # handle html requst
+            filename = filename[1:] + "." + filetype
+            print (f'try to open file {filename}')
+            with open(full_path + f"\{filename}", 'rb') as f:
                 file_data = f.read()
-                content_type = 'text/html; charset=UTF-8'  # Set appropriate content type
-                self.send_response(200)  # OK status code
-                self.send_header('Content-Type', content_type)
-                self.end_headers()
                 self.wfile.write(file_data)
         except FileNotFoundError:
             # Handle file not found case (404 Not Found)
